41bcae3ec3a5078f24de6c7c9191e1f02a4e2ed6
Antonio Romero <tonicb155@hotmail.com>
2024-04-24 20:33:32 +0200

    doEvento

--- controllers/evento.controller.js
+++ controllers/evento.controller.js
@@ -6,3 +6,20 @@ const Evento = require("../models/Evento.model")
 module.exports.evento = (req, res, next) => {
     res.render("evento");
 }
+module.exports.doEvento = (req, res, next) => {
+const renderWithErros = (errors, values) => {
+    res.render("evento", {errors, values} )
+}
+
+Evento.create(req.body)
+.then(() => {
+    res.redirect("/home");
+})
+.catch((err) => {
+    if(err instanceof mongoose.Error.ValidatorError){
+        renderWithErros(err.errors, req.body)
+    } else {
+        next(err)
+    }
+})
+}
